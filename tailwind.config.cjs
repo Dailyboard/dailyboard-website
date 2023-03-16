a5d1b83ea64e8ee7ad348a83977819f28029542c
adamkarminski <adamkarminski@gmail.com>
2023-03-16 21:44:06 +0100

    feat: install tailwind and vue

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./src/**/*.{astro,ts,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
