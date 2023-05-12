d494d097a6f26eebd50a4f2328faf1333fc1ffec
adamkarminski <adamkarminski@gmail.com>
2023-05-12 16:51:00 +0200

    minor: update log in link

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -62,7 +62,7 @@
 					</div>
 					<div class="py-6">
 						<a
-							href="#"
+							href="https://app.dailyboard.io/auth/login"
 							class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 hover:bg-gray-800"
 							>Log in</a
 						>
