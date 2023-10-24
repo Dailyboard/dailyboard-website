6f326c91c8c855eec2c2cc63d04a1c8f217321e0
adamkarminski <adamkarminski@gmail.com>
2023-10-24 21:23:14 +0200

    feat: use partytown to defer analytics to web worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), vue()],
+	integrations: [
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+		tailwind(),
+		vue(),
+	],
 	server: {
 		port: 3001,
 		host: true,
