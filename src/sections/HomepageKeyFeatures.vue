265d547fa8ad53c448adf2357ef03a17e5ba181d
adamkarminski <adamkarminski@gmail.com>
2023-10-28 07:18:52 +0200

    feat: improve narrative on how Dailyboard works

--- src/sections/HomepageKeyFeatures.vue
+++ src/sections/HomepageKeyFeatures.vue
@@ -1,94 +1,85 @@
 <template>
-	<div
-		id="how-it-works"
-		class="overflow-hidden py-24 sm:py-32 relative isolate"
-		ref="target"
-	>
-		<div
-			class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-		>
-			<svg
-				class="relative left-[calc(50%-11rem)] -z-10 mt-80 h-[21.1875rem] max-w-none -translate-x-1/4 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-				viewBox="0 0 1155 678"
-			>
-				<path
-					fill="url(#f4773080-2a16-4ab4-9fd7-579fec69a4f7)"
-					fill-opacity=".2"
-					d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-				></path>
-				<defs>
-					<linearGradient
-						id="f4773080-2a16-4ab4-9fd7-579fec69a4f7"
-						x1="1155.49"
-						x2="-78.208"
-						y1=".177"
-						y2="474.645"
-						gradientUnits="userSpaceOnUse"
-					>
-						<stop stop-color="#34d399"></stop>
-						<stop offset="1" stop-color="#0d9488"></stop>
-					</linearGradient>
-				</defs>
-			</svg>
-		</div>
+	<div id="how-it-works" class="overflow-hidden relative isolate" ref="target">
 		<div class="mx-auto max-w-7xl px-6 lg:px-8">
 			<div
-				class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2"
+				class="mx-auto max-w-2xl pb-24 sm:pb-32 lg:text-center"
+				data-animate="animate-fade-left"
 			>
-				<div class="lg:pr-8 lg:pt-4">
-					<div class="lg:max-w-lg">
-						<h2
-							class="text-base font-semibold leading-7 text-text-primary-weak"
-							data-animate="animate-fade-left"
-						>
-							Commit to succeed
-						</h2>
-						<p
-							class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl"
-							data-animate="animate-fade-left"
-						>
-							Rediscover teamwork
-						</p>
-						<p
-							class="mt-6 text-lg leading-8 text-text-weak"
-							data-animate="animate-fade-left"
-						>
-							Dailyboard provides a unique way to plan, monitor and synchronize
-							your team's
-							<strong>commitments</strong>.
-						</p>
-						<dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
-							<div
-								v-for="feature in features"
-								:key="feature.name"
-								class="relative pl-9"
-								data-animate="animate-fade-left"
-								:data-delay="feature.delay"
+				<h2 class="text-base font-semibold leading-7 text-text-primary-weak">
+					Commit to succeed
+				</h2>
+				<p class="mt-2 text-3xl font-bold tracking-tight text-text sm:text-4xl">
+					Rediscover teamwork
+				</p>
+				<p class="mt-6 text-lg leading-8 text-text-weak">
+					Dailyboard provides a unique way to plan your day as a team,
+					using&nbsp;the&nbsp;power&nbsp;of <strong>public&nbsp;commitments</strong>.
+				</p>
+			</div>
+
+			<div
+				v-for="feature in features"
+				:key="feature.name"
+				class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center pb-32 sm:pb-48 relative"
+			>
+				<div
+					class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+				>
+					<svg
+						class="relative left-[calc(50%-11rem)] -z-10 mt-80 h-[21.1875rem] max-w-none -translate-x-1/4 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+						viewBox="0 0 1155 678"
+					>
+						<path
+							fill="url(#f4773080-2a16-4ab4-9fd7-579fec69a4f7)"
+							fill-opacity=".2"
+							d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+						></path>
+						<defs>
+							<linearGradient
+								id="f4773080-2a16-4ab4-9fd7-579fec69a4f7"
+								x1="1155.49"
+								x2="-78.208"
+								y1=".177"
+								y2="474.645"
+								gradientUnits="userSpaceOnUse"
 							>
-								<dt class="inline font-semibold">
-									<component
-										class="absolute top-1 left-1 h-5 w-5 text-text-primary-weak"
-										:is="feature.icon"
-										aria-hidden="true"
-									/>
-									{{ feature.name }}
-								</dt>
-								{{ ' ' }}
-								<dd class="inline" v-html="feature.description"></dd>
-							</div>
-						</dl>
+								<stop stop-color="#34d399"></stop>
+								<stop offset="1" stop-color="#0d9488"></stop>
+							</linearGradient>
+						</defs>
+					</svg>
+				</div>
+				<div
+					:class="{ 'lg:order-last': feature.reversed }"
+					data-animate="animate-fade-left"
+					:data-delay="feature.reversed ? 600 : 0"
+				>
+					<div class="lg:max-w-lg">
+						<p
+							class="mt-2 text-xl font-bold tracking-tight sm:text-3xl flex items-center gap-6"
+						>
+							<component
+								class="h-6 w-6 text-text-primary-weak"
+								:is="feature.icon"
+								aria-hidden="true"
+							/>
+							{{ feature.name }}
+						</p>
+						<div
+							class="mt-6 text-lg leading-8 text-text-weak flex flex-col gap-3"
+							v-html="feature.description"
+						/>
 					</div>
 				</div>
 				<img
-					src="https://res.cloudinary.com/dl5xtsolh/image/upload/v1696697789/dailyboard_app_screenshot_board.png"
+					:src="feature.imageUrl"
 					alt="Dailyboard - Board screenshot"
-					class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
 					loading="lazy"
 					decoding="async"
 					data-animate="animate-fade-left"
-					data-delay="600"
-					width="2432"
-					height="1442"
+					:data-delay="feature.reversed ? 0 : 600"
+					width="576"
+					height="864"
 				/>
 			</div>
 		</div>
@@ -104,32 +95,38 @@
 
 	const features = [
 		{
-			name: 'Visualize the future.',
-			description:
-				'Your tasks reflect where things <strong><u>are</u></strong>. Commitments reflect where you want them <strong><u>to be</u></strong>.',
+			name: 'Bring everyone together',
+			description: `
+					<p>Everyone has to plan their day somehow - using a todo app, a piece of paper or just in their mind. Dailyboard simply provides a <strong>shared space</strong> for that.</p>
+					<p>Collecting the scattered plans in one place makes it possible to fully synchronize your work.</p>
+				`,
+			icon: SquaresPlusIcon,
+			delay: 100,
+			reversed: false,
+			imageUrl:
+				'https://res.cloudinary.com/dl5xtsolh/image/upload/v1698470048/KeyFeatures__Sync_bgyeh7.png',
+		},
+		{
+			name: 'Map your path to success',
+			description: `<p>Your tasks only show where things <strong>are</strong>. Commitments reflect where you want them <strong>to be</strong>.</p>
+				<p>In Dailyboard you visualize the desired state of your tasks by the end of a day. It gives you confidence everyone is on the same page.</p>`,
 			icon: RocketLaunchIcon,
 			delay: 200,
+			reversed: true,
+			imageUrl:
+				'https://res.cloudinary.com/dl5xtsolh/image/upload/v1698470047/KeyFeatures__Map_pf2nro.png',
 		},
 		{
-			name: 'Who, what, when.',
-			description:
-				'Stop wondering if anyone will work on that important thing today. Get your answers without wasting time on status updates',
+			name: 'Focus on the team',
+			description: `
+				<p>"How can our team create <strong>the most value</strong> in the next 24h?" - it\'s the most important question of daily planning.</p>
+				<p>With Dailyboard it\'s much easier to answer the question together - spotting roadblocks and opportunities for help.</p>
+			`,
 			icon: UserCircleIcon,
-			delay: 200,
-		},
-		{
-			name: 'Plan together.',
-			description:
-				'Quickly scan commitments of your teammates and spot roadblocks ahead of time.',
-			icon: SquaresPlusIcon,
 			delay: 300,
+			reversed: false,
+			imageUrl:
+				'https://res.cloudinary.com/dl5xtsolh/image/upload/v1698470047/KeyFeatures__Team_khcxdj.png',
 		},
-		// {
-		// 	name: 'Collect data and improve.',
-		// 	description:
-		// 		'Use commitments history and analytics to improve and elevate your performance.',
-		// 	icon: SparklesIcon,
-		// 	delay: 400,
-		// },
 	];
 </script>
